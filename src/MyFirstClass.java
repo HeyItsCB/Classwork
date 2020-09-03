552d95e104a738b4c5281593f33e64aed23e710c
Burkhard Chris <21burkhard@st-21burkharch.nc.rr.com>
2020-09-03 10:56:48 -0400

    Initial commit

---
+++ src/MyFirstClass.java
@@ -0,0 +1,7 @@
+public class MyFirstClass {
+    public static void main(String args[]) {
+        String name = "Chris";
+        System.out.println("Dark mode is for the cool kids.");
+        System.out.println("Written by: " + name);
+    }
+}
